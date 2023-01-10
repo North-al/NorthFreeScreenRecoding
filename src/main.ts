56e6c26d290d945efdb913491150bc143aac6a5b
North <1227379879@qq.com>
2023-01-10 21:14:58 +0800

    feat: 新增video

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './layouts'
 import { setupExtendDom } from './utils/extend-dom'
 import { setupStyles } from './styles'
+import videoBgImg from './assets/images/bg.png'
 
 setupExtendDom()
 setupStyles()
@@ -11,8 +12,8 @@ document.querySelector('#app')!.innerHTML = `
         <north-main>
             <div slot="default">
                 <north-content height="calc(100vh - 450px);" mh="500px">
-                <div slot="default">
-                    <video id="north-video"></video>
+                <div slot="default" class="full">
+                    <video id="north-video" class="full" poster=${videoBgImg}></video>
                 </div>
                 </north-content>
                 <div style="margin-top: 20px">
